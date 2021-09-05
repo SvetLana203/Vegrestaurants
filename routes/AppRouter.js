2a605164acac784ed5c14ead38f1560b01898bd2
SvetLana203 <shirapovauu@gmail.com>
2021-09-05 17:04:34 -0500

    search1 backend

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -5,5 +5,6 @@ const router = Router()
 router.post('/places', controllers.createPlace)
 router.get('/places', controllers.getAllPlaces)
 router.delete('/places/:id', controllers.deletePlace)
+router.get('/places/:id', controllers.getPlaceById)
 
 module.exports = router
